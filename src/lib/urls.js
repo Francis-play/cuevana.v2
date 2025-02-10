cbafd7eebb68bfc205d526444e3c2bdff3610883
Francis-play <93499490+Francis-play@users.noreply.github.com>
2025-02-09 21:06:02 -0400

    Create urls.js

---
+++ src/lib/urls.js
@@ -0,0 +1,10 @@
+module.exports = {
+  BASE_URL: "https://www.cuevana.is/_next/data/8R0H-ZmQSoVU5r0u8suVP/es",
+  MOVIES: [
+    "peliculas/page/",
+    "peliculas/tendencias/dia",
+    "peliculas/tendencias/semana",
+    "peliculas/estrenos",
+    "peliculas/estrenos/page/
+  ],
+};
