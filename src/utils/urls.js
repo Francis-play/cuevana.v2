5a065caaa9e0c2ac610f068ae69c87de49779927
Francis-play <93499490+Francis-play@users.noreply.github.com>
2025-02-10 13:40:30 -0400

    Update and rename urls.js to urls.js

---
+++ src/utils/urls.js
@@ -0,0 +1,36 @@
+module.exports = {
+  BASE_URL: "https://www.cuevana.is/_next/data/8R0H-ZmQSoVU5r0u8suVP/es",
+  MOVIES: [
+    "peliculas/page/",
+    "peliculas/tendencias/dia",
+    "peliculas/tendencias/semana",
+    "peliculas/estrenos",
+    "peliculas/estrenos/page/
+  ],
+
+  SERIES: [
+    "series",
+    "series/page",
+    "series/estrenos",
+    "series/estrenos/page,
+    "series/tendencias/dia",
+    "series/tendencias/semana"
+    ],
+
+  GENRES: [
+    "accion",
+    "animacion",
+    "aventura",
+    "ciencia-ficcion",
+    "comedia",
+    "crimen",
+    "drama",
+    "familiar",
+    "fantasia",
+    "misterio",
+    "romance",
+    "terror",
+    "suspenso",
+  ],
+};
+
